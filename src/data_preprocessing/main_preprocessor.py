9c458380220d44bf1b0b8cd9d473b4d5b96d2cf6
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2025-05-14 01:58:48 +0300

    Update main_preprocessor.py

--- src/data_preprocessing/main_preprocessor.py
+++ src/data_preprocessing/main_preprocessor.py
@@ -1,12 +1,7 @@
-import torch
 from preprocess import ASLPreprocessor
 
-RAW_DATA_PATH = "D:/path/to/asl_alphabet_train"
-PROCESSED_PATH = "D:/path/to/processed/asl_processed.pt"
+RAW_DATA_PATH = r'D:\GitHubRepo\ASL-coursework\src\data\raw\ASL_Alphabet_Dataset\asl_alphabet_train'
+PROCESSED_PATH = r'D:\GitHubRepo\ASL-coursework\src\data\processed\ASL_Alphabet_Dataset\asl_alphabet_train'
 
 preprocessor = ASLPreprocessor()
-preprocessor.preprocess_dataset(
-    r'D:\GitHubRepo\ASL-coursework\src\data\raw\ASL_Alphabet_Dataset\asl_alphabet_train',
-    r'D:\GitHubRepo\ASL-coursework\src\data\processed\ASL_Alphabet_Dataset\asl_alphabet_train')
-
-processed_data = torch.load(PROCESSED_PATH)
+preprocessor.preprocess_dataset(RAW_DATA_PATH,PROCESSED_PATH)
